a6d101a6a553e9cc08bc20d5fb259d3759b199e8
janath <janathhma@gmail.com>
2024-02-18 10:36:17 +0530

    allign forget password link in to center using bootstrap flexbox utilize class

--- src/login.jsx
+++ src/login.jsx
@@ -16,7 +16,9 @@ function Login() {
                         <label htmlFor="floatingPassword">Password</label>
                     </div>
                     <button className="btn btn-primary w-100 py-2  mt-3" type="submit">Login</button>
+                    <div className="d-flex justify-content-center mt-3">
                     <p><a href="#" class="link-primary">Forget password?</a></p>
+                    </div>
                     <hr />
                     <div className="d-flex justify-content-center">
                     <button className="w-50 btn btn-success py-2" type="submit">Create new account</button>
