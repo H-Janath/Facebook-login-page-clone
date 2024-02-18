5032d008317bc0dc2f856b339d8b539d3a77c023
janath <janathhma@gmail.com>
2024-02-18 09:38:49 +0530

    add bootstrap login style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import Login from './login'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <Login/>
     </>
   )
 }
