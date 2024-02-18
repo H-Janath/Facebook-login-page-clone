0316676066c0a946a64d3901027c4936ce5cbe2e
janath <janathhma@gmail.com>
2024-02-18 10:48:42 +0530

    content verticaly allign to center

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './login'
+import Side from './side'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -8,8 +9,8 @@ function App() {
     <>
     <div className="continer ">
       <div className="row">
-        <div className="col-lg-6">
-        
+        <div className="col-lg-6 ">
+          <Side/>
         </div>
         <div className="col-lg-6">
         <Login/>
