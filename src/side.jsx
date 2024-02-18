bc96b442f81550bc1d50fdc001774aa4f35322be
janath <janathhma@gmail.com>
2024-02-18 10:39:09 +0530

    add side component content

--- src/side.jsx
+++ src/side.jsx
@@ -1,5 +1,10 @@
 
 
 export default function Sider(){
-    
+    return(
+        <>
+        <h1>facebook</h1>
+        <p>connect with friends and the world around you on facebook</p>
+        </>
+    )
 }
