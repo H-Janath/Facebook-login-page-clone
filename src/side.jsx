2befd98874f5f7ad3089747b0e379654eabc1d1e
janath <janathhma@gmail.com>
2024-02-18 10:54:17 +0530

    add custom css for facebook logo

--- src/side.jsx
+++ src/side.jsx
@@ -1,9 +1,9 @@
-
+import './side.css'
 
 export default function Sider(){
     return(
         <>
-        <h1>facebook</h1>
+        <h1 className="text-primary">facebook</h1>
         <p>connect with friends and the world around you on facebook</p>
         </>
     )
