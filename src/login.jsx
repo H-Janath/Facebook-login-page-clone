cecaab74e626c01493677e121f3f4ded54abd47f
janath <janathhma@gmail.com>
2024-02-18 10:29:57 +0530

    allign create new button in to center using bootstrap flexbox utilize class

--- src/login.jsx
+++ src/login.jsx
@@ -7,20 +7,21 @@ function Login() {
         <>
             <div className="card p-5" style={{width:'450px',height:'auto'}}>
                 <form>
-
-                    <div className="form-floating" align-content-xl-center >
+                    <div className="form-floating  mt-3" align-content-xl-center >
                         <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
                         <label htmlFor="floatingInput">Email or phone number</label>
                     </div>
-                    <div className="form-floating">
+                    <div className="form-floating  mt-3">
                         <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
                         <label htmlFor="floatingPassword">Password</label>
                     </div>
-                    <button className="btn btn-primary w-100 py-2" type="submit">Login</button>
-                    <p><a href="#" class="link-primary">Primary link</a></p>
+                    <button className="btn btn-primary w-100 py-2  mt-3" type="submit">Login</button>
+                    <p><a href="#" class="link-primary">Forget password?</a></p>
                     <hr />
-                    <button className="w-50 btn btn-success w-100 py-2" type="submit">Create new account</button>
-
+                    <div className="d-flex justify-content-center">
+                    <button className="w-50 btn btn-success py-2" type="submit">Create new account</button>
+                    </div>
+                    
                 </form>
             </div>
         </>
